2acda670babd654f4b5788ece97e10c6fbc5d65e
Jessica Mulein <jessica@digitaldefiance.org>
2026-01-29 15:28:32 -0800

    v2.4.2

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,12 @@ module.exports = {
   roots: ['<rootDir>/tests'],
   testMatch: ['**/*.spec.ts', '**/*.test.ts'],
   modulePaths: ['<rootDir>'],
-  moduleDirectories: ['node_modules'],
+  moduleDirectories: ['node_modules', '<rootDir>/../../node_modules'],
   moduleNameMapper: {
     '^lru-cache$': '<rootDir>/__mocks__/lru-cache.js',
+    '^@digitaldefiance/i18n-lib$': '<rootDir>/../../dist/packages/digitaldefiance-i18n-lib/src/index.js',
+    '^@digitaldefiance/suite-core-lib$': '<rootDir>/../../dist/packages/digitaldefiance-suite-core-lib/src/index.js',
+    '^@digitaldefiance/ecies-lib$': '<rootDir>/../../dist/packages/digitaldefiance-ecies-lib/src/index.js',
   },
   setupFiles: ['<rootDir>/../../jest.setup.js'],
   collectCoverageFrom: [
