1d0cc83acb4efb74aab9a8bb50caf715ea463169
Jessica Mulein <jessica@digitaldefiance.org>
2026-02-15 12:44:08 -0800

    v2.4.14

--- scaffolding/lib/src/lib/interfaces/constants.ts
+++ scaffolding/lib/src/lib/interfaces/constants.ts
@@ -1,6 +1,12 @@
 import { IConstants as ISuiteCoreConstants } from '@digitaldefiance/suite-core-lib';
+import type { ISuiteCoreI18nConstants } from '@digitaldefiance/suite-core-lib';
 
-export interface IConstants extends ISuiteCoreConstants {
+/**
+ * Application constants interface.
+ * Extends ISuiteCoreConstants for general app config and
+ * ISuiteCoreI18nConstants for type-safe i18n template variables.
+ */
+export interface IConstants extends ISuiteCoreConstants, ISuiteCoreI18nConstants {
   readonly Site: string;
   readonly SiteTagline: string;
   readonly SiteDescription: string;
