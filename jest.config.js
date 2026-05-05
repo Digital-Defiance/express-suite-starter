e5a3bfb4d59d337971e9c22dbdbc4a9536ff0eb9
Jessica Mulein <jessica@digitaldefiance.org>
2026-05-05 11:34:29 -0700

    failed tests

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,11 @@ module.exports = {
     '^@digitaldefiance/i18n-lib$': '<rootDir>/../../dist/packages/digitaldefiance-i18n-lib/src/index.js',
     '^@digitaldefiance/suite-core-lib$': '<rootDir>/../../dist/packages/digitaldefiance-suite-core-lib/src/index.js',
     '^@digitaldefiance/ecies-lib$': '<rootDir>/../../dist/packages/digitaldefiance-ecies-lib/src/index.js',
+    // fast-check@4.x requires pure-rand@^8.x, but the root node_modules has pure-rand@7.x
+    // (used by jest-circus). Map all pure-rand subpath imports to the version nested under fast-check.
+    '^pure-rand/generator/(.*)$': '<rootDir>/../../node_modules/fast-check/node_modules/pure-rand/lib/generator/$1.js',
+    '^pure-rand/distribution/(.*)$': '<rootDir>/../../node_modules/fast-check/node_modules/pure-rand/lib/distribution/$1.js',
+    '^pure-rand/utils/(.*)$': '<rootDir>/../../node_modules/fast-check/node_modules/pure-rand/lib/utils/$1.js',
   },
   setupFiles: ['<rootDir>/../../jest.setup.js'],
   collectCoverageFrom: [
